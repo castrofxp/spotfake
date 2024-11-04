7892abc7c22c0e1f6186a70aa9164d5e96c4a3c6
GABRIEL CASTRO FERNANDES <gabriel_c_fernandes@estudante.sesisenai.org.br>
2024-11-04 12:20:40 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,89 @@
+import Express from 'express';
+import { User, criarTabelas } from './db.js'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import cors from 'cors'
+
+const app = Express()
+app.use(Express.json())
+app.use(cors())
+
+//criarTabelas() // --> usar so uma vez já que com o 'force: true' ele sempre apaga a tabela existente e cria uma nova do zero
+app.post('/registro', async function (req, res) {
+    // verificar se todos os campos foram enviados
+    try {
+        const { nome, sobrenome, email, senha, dataNascimento } = req.body
+        if (!nome || !sobrenome || !email || !senha || !dataNascimento) {
+            res.status(406).send('todos os campos devem ser preenchidos')
+            return
+        }
+
+        if (await User.findOne({ where: { email: email } })) {
+            res.status(400).send('usuario ja existente no sistema')
+            return
+        }
+
+        const senhaSegura = bcryptjs.hashSync(senha, 10)
+        const novoUsuario = User.create({
+            nome: nome,
+            sobrenome: sobrenome,
+            email: email,
+            senha: senhaSegura,
+            dataNascimento: dataNascimento,
+        })
+        res.status(200).send('ok, usuario criado')
+    } catch (erro) {
+        console.log(erro)
+    }
+})
+
+app.post('/login', async function (req, res) {
+// validar informações
+
+    try {
+        const { email, senha } = req.body
+        if (!email || !senha) {
+            res.status(406).send('todos os campos devem ser preenchidos')
+            return
+        }
+// verificar a existencia do usuario
+
+        const usuario = await User.findOne({ where: { email: email } })
+        if (!usuario) {
+            res.status(404).send('este usuario não está cadastrado')
+            return
+        }
+
+// compara a senha enviada com a senha do banco de dados
+
+        const senhaCorreta = bcryptjs.compareSync(senha, usuario.senha)
+        if(!senhaCorreta) {
+            res.status(403).send('senha incorreta')
+            return
+        }
+// criar eum token de autenticação
+        const token = jwt.sign(
+            {
+                nome:usuario.nome,
+                email: usuario.email,
+                status: usuario.status
+            },
+            'chavecriptografiasupersegura',
+            {
+                expiresIn: "30d"
+            }
+        )
+
+        console.log(token) 
+
+        // devolver a resposta com o token  
+
+        res.status(200).send({msg: 'voce foi logado', token: token})
+        
+} catch (erro) {
+        console.log(erro)
+        res.status(500).send('houve um problema')
+    }
+})
+
+app.listen(8000)
